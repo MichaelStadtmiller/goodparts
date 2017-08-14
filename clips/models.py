337f227ebdeff7204ac5d1370b45b924bb7a71a4
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2017-08-13 22:42:50 -0400

    fixed untracked, added models

--- clips/models.py
+++ clips/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    name_path = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, null=True)
+    poster = models.CharField(max_length=500, null=True)
+    studio = models.CharField(max_length=100, null=True)
+    genres = models.TextField(null=True)
+    year_released = models.integer(null=True)
