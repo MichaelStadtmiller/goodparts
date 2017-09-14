c8588973a7a8e8769c1a908e825f1e493b0160a0
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2017-09-14 11:19:22 -0400

    update alphabet and debugging

--- clips/getklips.py
+++ clips/getklips.py
@@ -1,18 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
-from .models import decades
+from .models import decades, alphabet
 
 
 def getURLS():
     for d in decades:
-        a = gethtml(d)
-    return (a)
+        for a in alphabet:
+            myhtml = gethtml(d, a)
+    return (myhtml)
 
 
-def gethtml(d):
-    baseURL = "http://klipd.com/decade/"
-    url = baseURL+d
-    print(url)
+def gethtml(d, a):
+    base_url = "http://klipd.com/decade"
+    url = base_url + "/" + d + "/" + a
     headers = {'Accept': 'text/css,*/*;q=0.1',
                'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
                'Accept-Encoding': 'gzip,deflate,sdch',
@@ -22,17 +22,26 @@ def gethtml(d):
     soup = BeautifulSoup(html.text, "html.parser")
 
     # get the total movies for the decade
-    total_movies_count = soup.find('h1', attrs={'class': 'entry-title'}).string.split(' ', 1)[0]
-    print(total_movies_count)
+    try:
+        total_movies_count = soup.find('h1', attrs={'class': 'entry-title'}).string.split(' ', 1)[0]
+    except:
+        print("nope")
+    if total_movies_count == "Movies":
+        total_movies_count = 0
+    else:
+        total_movies_count = int(total_movies_count)
 
     # count all movies on the page
     current_movie_count = len(soup.find_all('div', attrs={'class': 'poster'}))
 
-    while current_movie_count < total_movies_count:
+    if current_movie_count < total_movies_count:
+        print(url)
+        print("total: %s current: %s", str(total_movies_count), str(current_movie_count))
+    #while current_movie_count < total_movies_count:
         # click the link
 
-        current_movie_count = len(soup.find_all('div', attrs={'class': 'poster'}))
+    #    current_movie_count = len(soup.find_all('div', attrs={'class': 'poster'}))
 
-    posters = soup.find_all('div', attrs={'class': 'poster'})
+    #posters = soup.find_all('div', attrs={'class': 'poster'})
 
-    return posters
+    return soup
